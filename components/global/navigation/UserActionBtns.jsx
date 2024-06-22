0f2a0ce180154e8642254bc49eefa73c65c8afa8
Taseen Jeem <taseenjeem.dev@gmail.com>
2024-06-23 01:16:40 +0600

    minor changes

--- components/global/navigation/UserActionBtns.jsx
+++ components/global/navigation/UserActionBtns.jsx
@@ -1,11 +1,17 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const UserActionBtns = () => {
+  const pathName = usePathname();
+
   return (
     <>
       <Link
         href={`/login`}
-        className="btn btn-primary btn-sm md:mx-1 mx-0 my-1 md:my-0"
+        className={`btn btn-primary ${
+          pathName !== "/login" && "btn-outline"
+        } btn-sm md:mx-1 mx-0 my-1 md:my-0`}
       >
         Log In
       </Link>
