e5197b5310f51b62e6b22eeb1f29191bd419b07d
Taseen Jeem <taseenjeem.dev@gmail.com>
2024-06-24 15:48:25 +0600

    added hook form in reg

--- components/page/register-page/RegisterForm.jsx
+++ components/page/register-page/RegisterForm.jsx
@@ -1,10 +1,45 @@
+"use client";
 import Logo from "@/components/global/logo/Logo";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const RegisterForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm();
+
+  const registerForm = async (data) => {
+    setIsLoading(true);
+    try {
+      console.log(data);
+    } catch (error) {
+      setError(error?.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
-      <form className="card-body pb-0">
+      <form onSubmit={handleSubmit(registerForm)} className="card-body pb-0">
         <Logo formMode={true} />
+        <div className="form-control mt-6">
+          {isLoading ? (
+            <button disabled className="btn btn-primary">
+              <span className="loading loading-spinner"></span>
+              Loading..
+            </button>
+          ) : (
+            <button type="submit" className="btn btn-primary">
+              Create an account
+            </button>
+          )}
+        </div>
       </form>
     </>
   );
