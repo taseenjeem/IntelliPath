148f54897b3df3cb114c5b14e41b14957e2fa5ef
Taseen Jeem <taseenjeem.dev@gmail.com>
2024-06-27 00:11:11 +0600

    added instructor populate

--- database/queries/courseQueries.js
+++ database/queries/courseQueries.js
@@ -1,9 +1,12 @@
 import { categoryDataModel } from "../models/category-model";
 import { coursesDataModel } from "../models/course-model";
+import { userDataModel } from "../models/user-model";
 
 export const getAllCourses = async () => {
   const allCourses = await coursesDataModel
     .find({})
-    .populate({ path: "category", model: categoryDataModel });
+    .populate({ path: "category", model: categoryDataModel })
+    .populate({ path: "instructor", model: userDataModel });
+
   return allCourses;
 };
