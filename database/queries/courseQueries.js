ab732dd3b2fad3445cd0e0b47d3d2b5c36fbe95f
Taseen Jeem <taseenjeem.dev@gmail.com>
2024-06-26 23:59:03 +0600

    added populate method in course queries

--- database/queries/courseQueries.js
+++ database/queries/courseQueries.js
@@ -1,6 +1,9 @@
+import { categoryDataModel } from "../models/category-model";
 import { coursesDataModel } from "../models/course-model";
 
 export const getAllCourses = async () => {
-  const allCourses = await coursesDataModel.find({}).lean();
+  const allCourses = await coursesDataModel
+    .find({})
+    .populate({ path: "category", model: categoryDataModel });
   return allCourses;
 };
