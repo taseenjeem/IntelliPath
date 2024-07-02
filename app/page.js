4e0681e38f9b7afb6dd94457affbe18da513b00e
Taseen Jeem <taseenjeem.dev@gmail.com>
2024-07-03 01:52:16 +0600

    added headline

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import CategoriesList from "@/components/page/landing-page/CategoriesList";
 import Hero from "@/components/page/landing-page/Hero";
+import Testimonial from "@/components/page/landing-page/Testimonial";
 import TopCourses from "@/components/page/landing-page/TopCourses";
 
 const LandingPage = () => {
@@ -8,6 +9,7 @@ const LandingPage = () => {
       <Hero />
       <CategoriesList />
       <TopCourses />
+      <Testimonial />
     </>
   );
 };
---
+++ components/page/landing-page/Testimonial.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+const Testimonial = () => {
+  return (
+    <>
+      <section className="min-h-screen w-full wrapper mt-5 md:mt-20">
+        <div className="md:flex md:items-end md:justify-between">
+          <div className="max-w-2xl">
+            <h2 className="lg:text-5xl text-3xl text-primary uppercase font-bold">
+              Voices of Success
+            </h2>
+
+            <p className="mt-6">
+              Hear from our vibrant community! Discover inspiring testimonials
+              and reviews from students and instructors who have experienced the
+              transformative power of IntelliPath. See how our courses have made
+              a real difference in their educational journeys and career
+              advancements.
+            </p>
+          </div>
+          <Link href="/shop" className="mt-6 md:mt-0 btn btn-primary ">
+            Join Our Community
+          </Link>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Testimonial;
