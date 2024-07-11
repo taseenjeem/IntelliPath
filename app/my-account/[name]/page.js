38d1b233168644a5cf24e3af6b7b9b2a34ddc045
Taseen Jeem <taseenjeem.dev@gmail.com>
2024-07-12 02:40:53 +0600

    added tabs

--- app/my-account/[name]/page.js
+++ app/my-account/[name]/page.js
@@ -1,5 +1,18 @@
+import Courses from "@/components/page/my-account-page/Courses";
+import ProfileInfo from "@/components/page/my-account-page/ProfileInfo";
+
 const MyAccountPage = () => {
-  return <>Hello World</>;
+  return (
+    <section className="custom-min-h wrapper w-full">
+      <h1 className="lg:text-4xl text-3xl text-primary uppercase font-semibold mt-5">
+        My Profile
+      </h1>
+      <div role="tablist" className="tabs tabs-bordered w-full mt-5">
+        <ProfileInfo />
+        <Courses />
+      </div>
+    </section>
+  );
 };
 
 export default MyAccountPage;
