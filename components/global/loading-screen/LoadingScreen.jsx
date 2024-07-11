c7d2898f349d9a4d0cbd3970090fcf0d9b51107d
Taseen Jeem <taseenjeem.dev@gmail.com>
2024-07-11 17:19:17 +0600

    minor changes

--- components/global/loading-screen/LoadingScreen.jsx
+++ components/global/loading-screen/LoadingScreen.jsx
@@ -2,7 +2,7 @@ const LoadingScreen = () => {
   return (
     <>
       <section className="custom-min-h w-full flex flex-col justify-center items-center">
-        <span className="loading loading-infinity w-20" />
+        <span className="loading loading-infinity w-28" />
         <h1 className="text-xl">Loading...</h1>
       </section>
     </>
