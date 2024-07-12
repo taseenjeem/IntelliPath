7d2fd3c7aa180b51585d246c2a7b0496e6c31d00
Taseen Jeem <taseenjeem.dev@gmail.com>
2024-07-13 02:03:56 +0600

    added state controlled of profile info form

--- components/page/my-account-page/ProfileInfoForm.jsx
+++ components/page/my-account-page/ProfileInfoForm.jsx
@@ -1,129 +1,178 @@
+"use client";
+import { useState, useEffect } from "react";
+
 const getCountries = async () => {
   return import("/database/json/countries.json").then(
     (module) => module.default
   );
 };
 
-const ProfileInfoForm = async () => {
-  const allCountries = await getCountries();
+const ProfileInfoForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    username: "",
+    gender: "",
+    birthDate: "",
+    country: "",
+    email: "",
+    phone: "",
+    address: "",
+  });
+
+  const [allCountries, setAllCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const countries = await getCountries();
+      setAllCountries(countries);
+    };
+    fetchCountries();
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
 
   return (
-    <>
-      <div className="bg-base-300 card card-compact mt-5 md:mt-16">
-        <form action="" className="card-body">
-          <div className="grid grid-cols-1 gap-5 md:grid-cols-4">
-            <div className="form-control">
-              <label className="label" htmlFor="name">
-                <span className="label-text">Full Name</span>
-              </label>
-              <input
-                id="name"
-                name="name"
-                type="text"
-                className="input input-bordered"
-                defaultValue={`Md. Ta-Seen Fuad Jeem`}
-              />
-            </div>
-            <div className="form-control">
-              <label className="label" htmlFor="username">
-                <span className="label-text">Username</span>
-              </label>
-              <input
-                id="username"
-                name="username"
-                type="text"
-                className="input input-bordered"
-                defaultValue={`taseenjeem`}
-              />
-            </div>
-            <div className="form-control">
-              <label className="label" htmlFor="gender">
-                <span className="label-text">Gender</span>
-              </label>
-              <select
-                id="gender"
-                name="gender"
-                className="select select-bordered"
-              >
-                <option disabled selected>
-                  Select your gender
+    <div className="bg-base-300 card card-compact mt-5 md:mt-16">
+      <form onSubmit={handleSubmit} className="card-body">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-4">
+          <div className="form-control">
+            <label className="label" htmlFor="name">
+              <span className="label-text">Full Name</span>
+            </label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              className="input input-bordered"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="username">
+              <span className="label-text">Username</span>
+            </label>
+            <input
+              id="username"
+              name="username"
+              type="text"
+              className="input input-bordered"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="gender">
+              <span className="label-text">Gender</span>
+            </label>
+            <select
+              id="gender"
+              name="gender"
+              className="select select-bordered"
+              value={formData.gender}
+              onChange={handleChange}
+            >
+              <option disabled value="">
+                Select your gender
+              </option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Others">Others</option>
+            </select>
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="birthDate">
+              <span className="label-text">Date of birth</span>
+            </label>
+            <input
+              id="birthDate"
+              name="birthDate"
+              type="date"
+              className="input input-bordered"
+              value={formData.birthDate}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="country">
+              <span className="label-text">Your Country</span>
+            </label>
+            <select
+              id="country"
+              name="country"
+              className="select select-bordered"
+              value={formData.country}
+              onChange={handleChange}
+            >
+              <option value="" disabled>
+                Please select your country
+              </option>
+              {allCountries.map((item) => (
+                <option key={item.code} value={item.name}>
+                  {item.name}
                 </option>
-                <option>Male</option>
-                <option>Female</option>
-                <option>Others</option>
-              </select>
-            </div>
-            <div className="form-control">
-              <label className="label" htmlFor="birthDate">
-                <span className="label-text">Date of birth</span>
-              </label>
-              <input
-                id="birthDate"
-                name="birthDate"
-                type="date"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label" htmlFor="country">
-                <span className="label-text">Your Country</span>
-              </label>
-              <select
-                id="country"
-                name="country"
-                className="select select-bordered "
-              >
-                <option selected>Please select your country</option>
-                {allCountries.map((item) => (
-                  <option key={item.code}>{item.name}</option>
-                ))}
-              </select>
-            </div>
-            <div className="form-control">
-              <label className="label" htmlFor="email">
-                <span className="label-text">Your Email</span>
-              </label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                className="input input-bordered"
-                defaultValue={`mdtaseenfuad@gmail.com`}
-              />
-            </div>
-            <div className="form-control">
-              <label className="label" htmlFor="phone">
-                <span className="label-text">Your Phone Number</span>
-              </label>
-              <input
-                id="phone"
-                name="phone"
-                type="number"
-                min={0}
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label" htmlFor="address">
-                <span className="label-text">Your Address</span>
-              </label>
-              <input
-                id="address"
-                name="address"
-                type="text"
-                min={0}
-                className="input input-bordered"
-              />
-            </div>
+              ))}
+            </select>
           </div>
-          <div className="card-actions justify-end mt-5">
-            <button className="btn btn-primary btn-outline">
-              Save Information
-            </button>
+          <div className="form-control">
+            <label className="label" htmlFor="email">
+              <span className="label-text">Your Email</span>
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              className="input input-bordered"
+              value={formData.email}
+              onChange={handleChange}
+            />
           </div>
-        </form>
-      </div>
-    </>
+          <div className="form-control">
+            <label className="label" htmlFor="phone">
+              <span className="label-text">Your Phone Number</span>
+            </label>
+            <input
+              id="phone"
+              name="phone"
+              type="number"
+              min={0}
+              className="input input-bordered"
+              value={formData.phone}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="address">
+              <span className="label-text">Your Address</span>
+            </label>
+            <input
+              id="address"
+              name="address"
+              type="text"
+              className="input input-bordered"
+              value={formData.address}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="card-actions justify-end mt-5">
+          <button type="submit" className="btn btn-primary btn-outline">
+            Save Information
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
