7a408f1144feee1ca93db3e0a7067f34d1dce89c
Taseen Jeem <taseenjeem.dev@gmail.com>
2024-07-12 17:08:23 +0600

    minor changes

--- components/page/my-account-page/ProfileInfoTab.jsx
+++ components/page/my-account-page/ProfileInfoTab.jsx
@@ -14,7 +14,7 @@ const ProfileInfoTab = () => {
         defaultChecked
       />
       <div role="tabpanel" className="tab-content w-full mt-5">
-        <div className="mt-10 flex items-center md:gap-7 gap-4">
+        <div className="mt-10 flex justify-center md:justify-start items-center md:gap-7 gap-4">
           <UserAvatar />
           <div className="md:space-y-2 space-y-1">
             <h2 className="md:text-2xl text-xl text-primary font-semibold">
